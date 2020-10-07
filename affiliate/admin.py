8004f6dcf9997de8635be7c013b9406572f82898
jangascodingplace <janisgoesser92@gmail.com>
2020-10-07 02:00:33 +0200

    Cleanup

--- affiliate/admin.py
+++ affiliate/admin.py
@@ -1,21 +1,5 @@
 from django.contrib import admin
-from django.urls import reverse
-from django.utils.html import format_html
-from .models import Link, Client
-
-
-class ClientInline(admin.TabularInline):
-    model = Client
-    readonly_fields = ('ua_string', 'creation_date',)
-
-    def has_add_permission(self, request, obj=None):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
+from .models import Link
 
 
 class LinkAdmin(admin.ModelAdmin):
@@ -23,7 +7,6 @@ class LinkAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'title', 'request_count', 'creator', )
     search_fields = ('key', 'link', 'title', )
     list_filter = ('creator', )
-    inlines = (ClientInline, )
 
     readonly_fields = (
         'creation_date', 'request_count',
@@ -31,16 +14,3 @@ class LinkAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Link, LinkAdmin)
-
-
-class ClientAdmin(admin.ModelAdmin):
-    model = Client
-    list_display = (
-        'link', 'creation_date', 'device', 'browser', 'os'
-    )
-    readonly_fields = (
-        'link', 'creation_date', 'device', 'browser', 'os', 'ip', 'ua_string',
-    )
-
-
-admin.site.register(Client, ClientAdmin)
